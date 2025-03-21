bd9c1ff4a0769acc935de879d7875ffe8b93e2eb
justputoff <kholid.hasibuan75@gmail.com>
2025-03-21 12:48:45 +0700

    Update tool-detail.blade.php

--- resources/views/pages/frontend/tool-detail.blade.php
+++ resources/views/pages/frontend/tool-detail.blade.php
@@ -58,11 +58,16 @@
 }
 .tool-swiper .swiper-slide {
     text-align: center;
-    height: 500px;
+    height: 700px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 .tool-swiper img {
-    width: 100%;
-    height: 100%;
+    max-width: 100%;
+    max-height: 100%;
+    width: auto;
+    height: auto;
     object-fit: contain;
     border-radius: 8px;
 }
