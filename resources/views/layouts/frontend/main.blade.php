175a632da55cd0883b436ece050bc97071dc54de
justputoff <kholid.hasibuan75@gmail.com>
2025-03-21 14:15:39 +0700

    Update main.blade.php

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -34,7 +34,7 @@
       }
 
       body > div.container.my-5 > div > div > div > div > div.announcement-content > figure > img {
-        max-width: 900px;
+        max-width: 1080px;
       }
     </style>
     @stack('styles')
